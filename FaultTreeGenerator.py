0e9da76c4bf0cbd2214057909d9ed42b0b299570
William Dower <wdow95@hotmail.com>
2017-03-09 21:59:05 -0500

    Fixed logic for writing maxino input -- before it was finding a mincut of the vulnerabilities themselves, not the mincut of the components.

--- FaultTreeGenerator.py
+++ FaultTreeGenerator.py
@@ -3,19 +3,23 @@ import argparse
 import re
 import os
 import sys
-import subprocess
+from tempfile import NamedTemporaryFile
 from sets import Set
+from subprocess import check_output, CalledProcessError
 
 def sanity_check(args):
-    """Check to ensure infile exists, outfile is writeable"""
+    """Check to ensure infile exists, outfile is writeable, and SAT-solver is useable"""
     if not os.path.isfile(args.infile):
         sys.exit("ERROR: Infile '" + args.infile + "' does not exist.")
         
     if not os.access(args.infile, os.R_OK):
         sys.exit("ERROR: User does not have permission to read file '" + args.infile +"'")
     
-    if not os.access(args.outfile, os.W_OK):
+    if os.path.isfile(args.outfile) and not os.access(args.outfile, os.W_OK):
         sys.exit("ERROR: User does not have permission to write to '" + args.outfile + "'")
+        
+    if not os.access(args.satsolver, os.X_OK):
+        sys.exit("ERROR: User does not have permission to run SAT-solver '" + args.satsolver +"'")
         
         
 #end sanity_check
@@ -27,7 +31,7 @@ def main(args):
     components = '\{(?P<uid>.+), "(?P<comp_name>.+)", "(?P<IP>.+)", vul="(?P<vul>.+)"\}'
     vulnerabilities = '{name="(?P<vul_name>.+)" score="(?P<weight>\d+)"}'
 
-    fin = open(args.input)
+    fin = open(args.infile)
     info = fin.read()
 
     paths = re.findall(network_paths, info)
@@ -38,43 +42,104 @@ def main(args):
 
     path_vulnerabilities = []
 
+    comp_tracker = 1
+    comps_to_ints = {}
+    comps_to_weights = {}
+
     for path in paths:
         path_vuls = Set([])
         path_comps = [x for x in path[2].split(',')]
         path_comps.append(path[0])
-        print path_comps
+        
+        for comp in path_comps:
+            if comp not in comps_to_ints:
+                comps_to_ints[comp] = comp_tracker
+                comp_tracker += 1
+        #print "map: "
+        #print comps_to_ints
+        
+        #need to add up vuls associated with each comp to get a weight for that comp
+        
+        #print path_comps
         #grab vulnerabilities of the path itself
         for comp in comps:
+            weight = 0
             if(comp[0] in path_comps):
                 comp_vuls = comp[3].split(',')
                 for cv in comp_vuls:
+                    
                     for vul in vuls:
                         if(cv == vul[0]):
-                            path_vuls.add(str(vuls.index(vul) + 1))
+                            weight += int(vul[1])
+                            #print weight
+                    comps_to_weights[comp[0]] = weight
+        
+        path_vulnerabilities.append(path_comps)
                         
-        path_vulnerabilities.append(path_vuls)
+    print "int tags: "
+    print comps_to_ints
+    print "weights: "
+    print comps_to_weights
     
-    print path_vulnerabilities
+        
+    
+    #print path_vulnerabilities
 
-    num_variables = str(len(vuls))
-    num_clauses = str(len(vuls) + len(path_vulnerabilities))
-    hard_weight = str(sum(int(x[1]) for x in vuls) + 1)
+    num_variables = str(len(comps_to_ints))
+    num_clauses = str(len(comps_to_ints) + len(path_vulnerabilities))
+    hard_weight = str(sum(int(vul[1]) for vul in vuls ) + 1)
+    
+    #print hard_weight
 
-    print num_variables, num_clauses, hard_weight
+    #print num_variables, num_clauses, hard_weight
 
-    mi = open("maxino_input", "w")
+    #now generate maxino input as a tempfile
+    mi = NamedTemporaryFile()
 
     mi.write("p wcnf " + num_variables + " " + num_clauses + " "
         + hard_weight + "\n")
     
+    #write the components, preappended with the hard weight
     for path in path_vulnerabilities:
-        mi.write(hard_weight + " " + " ".join(path) + " 0\n")
+        mi.write(hard_weight)
+        for comp in path:
+            mi.write(" " + str(comps_to_ints[comp]))
+        mi.write(" 0\n")
+        #mi.write(hard_weight + " " + " ".join(path) + " 0\n")
     
-    for vul in vuls:
-        mi.write(str(vul[1]) + " " + "-" + str(vuls.index(vul) + 1) + " 0\n")
-    
-    mi.close()
+    #write the clauses wieghting each component, preappended by hard weight minus component weight
+    for comp in comps:
+        mi.write(str(int(hard_weight) - comps_to_weights[comp[0]]) + " -"
+            + str(comps_to_ints[comp[0]]) + " 0\n")
+        #mi.write(str(vul[1]) + " " + "-" + str(vuls.index(vul) + 1) + " 0\n")
+        
+    #now run the maxino SAT-solver on the tempfile to find a risk group,
+    #then append a line to the tempfile to remove the old risk group from
+    #consideration
+    optimum_result = 's OPTIMUM FOUND\nv (?P<opt>.+)'
+    out = open(args.outfile, "w")
 
+    mi.seek(0)
+    test= mi.read()
+    print test
+    mi.seek(0)
+    #run maxino SAT-solver on the tempfile
+    try:
+        maxino_output = check_output(["./maxino-2015-k16-static", mi.name])
+    except CalledProcessError as ex:
+        maxino_output = ex.output
+        returncode = ex.returncode
+        if returncode != 10: # some other error happened
+            raise
+    
+    rg = re.findall(optimum_result, maxino_output)
+    
+    print rg
+    component_config = [int(x) for x in rg[0].split(" ")]
+    #for comp in 
+    
+    #close tempfile, which deletes it
+    mi.close()
 
 #end main
 
@@ -93,10 +158,16 @@ if __name__ == "__main__":
                         type=str,
                         default="risk_groups",
                         help='''Name an output file to store the risk group data.''')
-    parser.add_argument('--size',
+    parser.add_argument('--satsolver',
                         '-s',
+                        type=str,
+                        default="maxino-2015-k16-static",
+                        help='''Name a version of the Maxino SAT-solver to use
+                         in processing the dependency information.''')
+    parser.add_argument('--reps',
+                        '-r',
                         type=int,
-                        default=10,
+                        default=3,
                         help='''Specify the number of risk groups that should be found.''')
     args = parser.parse_args()
     
