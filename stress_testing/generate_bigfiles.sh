eb9f09820f2a7e11ae068241a0abda56ede597a5
wdow <wdow95@hotmail.com>
2017-05-04 00:11:40 -0400

    Update and rename  generate_bigfiles.sh to generate_bigfiles.sh

--- stress_testing/generate_bigfiles.sh
+++ stress_testing/generate_bigfiles.sh
@@ -7,12 +7,12 @@ python bigfile-generator bf3
 
 echo bf1 run > timelog
 
-time python FaultTreeGeneratorModified.py bf1 -o output1 -r 10000 2>> timelog
+time ./ft_analyzer bf1 -o output1 -r 10000 2>> timelog
 
 echo bf2 run >> timelog
 
-time python FaultTreeGeneratorModified.py bf2 -o output2 -r 10000 2>> timelog
+time ./ft_analyzer bf2 -o output2 -r 10000 2>> timelog
 
 echo bf3 run >> timelog
 
-time python FaultTreeGeneratorModified.py bf3 -o output3 -r 10000 2>> timelog
+time ./ft_analyzer bf3 -o output3 -r 10000 2>> timelog
